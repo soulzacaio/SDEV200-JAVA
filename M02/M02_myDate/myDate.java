cce5a65f90eca049a804847911d93dbb11d70c97
soulzacaio <130192455+soulzacaio@users.noreply.github.com>
2023-11-03 17:51:22 -0400

    ds

--- M02/M02_myDate/myDate.java
+++ M02/M02_myDate/myDate.java
@@ -1,17 +1,14 @@
-/* 
-* Program name: myDate.java
-* Author: Caio Souza
-* Date last updated: 10/28/2023
-* Purpose: Create a program that takes two 2d arrays of data and compares if arrays are equal
-*/
+/*
+ * Program name: myDate.java Author: Caio Souza Date last updated: 10/28/2023 Purpose: define a date
+ * class using gregorian calendar
+ */
 
 import java.util.GregorianCalendar;
 
 public class myDate {
 
-    public String[] strMonths = { "January", "February", "March", "April",
-            "May", "June", "July", "August",
-            "September", "October", "November", "December" };
+    public String[] strMonths = {"January", "February", "March", "April", "May", "June", "July",
+            "August", "September", "October", "November", "December"};
 
     private int year;
     private String month;
@@ -63,6 +60,7 @@ public class myDate {
         day = eT.get(GregorianCalendar.DAY_OF_MONTH);
     }
 
+    // gets a day as a param and formats output to be printed
     public static String getSuffix(int day) {
         String suffix;
 
@@ -91,41 +89,29 @@ public class myDate {
         myDate d3 = new myDate(eT1);// assignment test date
         myDate d4 = new myDate(2023, 10, 23);// day classes started
 
-        System.out.println("Date #1: " + d1.getMonth() + " " + d1.getDay() + getSuffix(d1.getDay()) + ", "
-                + d1.getYear() + " -> current date, using default constr.");
-        System.out.println("Date #2: " + d2.getMonth() + " " + d2.getDay() + getSuffix(d2.getDay()) + ", "
-                + d2.getYear() + " assignment required date, using myDate(long)");
-        System.out.println("Date #3: " + d3.getMonth() + " " + d3.getDay() + getSuffix(d3.getDay()) + ", "
-                + d3.getYear() + " assignment test date, output: 9/18/1987, using myDate(long)");
-        System.out.println("Date #4: " + d4.getMonth() + " " + d4.getDay() + getSuffix(d4.getDay()) + ", "
-                + d4.getYear() + " day class started, using myDate(int, int, int)");
+        System.out.println("Date #1: " + d1.getMonth() + " " + d1.getDay() + getSuffix(d1.getDay())
+                + ", " + d1.getYear() + " -> current date, using default constr.");
+        System.out.println("Date #2: " + d2.getMonth() + " " + d2.getDay() + getSuffix(d2.getDay())
+                + ", " + d2.getYear() + " assignment required date, using myDate(long)");
+        System.out.println("Date #3: " + d3.getMonth() + " " + d3.getDay() + getSuffix(d3.getDay())
+                + ", " + d3.getYear()
+                + " assignment test date, output: 9/18/1987, using myDate(long)");
+        System.out.println("Date #4: " + d4.getMonth() + " " + d4.getDay() + getSuffix(d4.getDay())
+                + ", " + d4.getYear() + " day class started, using myDate(int, int, int)");
 
     }
 }
 
 /*
- * @startuml
- * !theme crt-amber
- * skinparam classAttributeIconSize 0
+ * @startuml !theme crt-amber skinparam classAttributeIconSize 0
  * 
- * class myDate
- * {
- * {field}
- * -year : int
- * -month : int
- * -day : int
+ * class myDate { {field} -year : int -month : int -day : int
  * 
- * {method}
- * +myDate()
- * +myDate(elapsedTime : long)
- * +myDate(year : int, month : int, day : int)
+ * {method} +myDate() +myDate(elapsedTime : long) +myDate(year : int, month : int, day : int)
  * 
- * +getYear() : int
- * +getMonth() : int
- * +getDay() : int
+ * +getYear() : int +getMonth() : int +getDay() : int
  * 
- * +setDate(elapsedTime : long) : void
- * }
+ * +setDate(elapsedTime : long) : void }
  * 
  * @enduml
  */
